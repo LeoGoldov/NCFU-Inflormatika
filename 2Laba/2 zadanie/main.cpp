4439ff1f62ae0b7a165f3342b3da2e2cc29db6f1
LeoGoldov <levfeb@gmail.com>
2025-04-01 13:33:57 +0300

    Update main.cpp

--- 2Laba/2 zadanie/main.cpp
+++ 2Laba/2 zadanie/main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include "Header.h"
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "RU");
+    Time time1;  
+    int hour, minute, second;  
+    cout << "Введите час: ";
+    cin >> hour;
+    cout << "Введите минуту: ";
+    cin >> minute;
+    cout << "Введите секунду: ";
+    cin >> second;
 
+    Time time2(hour, minute, second);  
+    cout << "Время : " << time2.toString() << endl;  
+    cout << "Количество секунд во времени 2: " << time2.totalSeconds() << endl;  
+
+    time2.incrementSeconds(5); 
+    cout << "После увеличения времени на 5 секунд: " << time2.toString() << endl;  
+
+    return 0;
+}
