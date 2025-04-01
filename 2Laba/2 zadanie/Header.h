064ab86878af17f952488323fab182c48e75c224
LeoGoldov <levfeb@gmail.com>
2025-04-01 13:34:57 +0300

    Update Header.h

--- 2Laba/2 zadanie/Header.h
+++ 2Laba/2 zadanie/Header.h
@@ -1,1 +1,28 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class Time {
+private:
+    int hour;
+    int minute;
+    int second;
+
+public:
+    // Конструктор по умолчанию (время 00:00:00)
+    Time();
+
+    // Конструктор с параметрами (часы, минуты, секунды)
+    Time(int h, int m, int s);
+
+    // Деструктор
+    ~Time();
+
+    long long totalSeconds() const;
+
+    void incrementSeconds(int secondsToAdd);
+
+    std::string toString() const; // Укажем явно std::string
+};
+
 
