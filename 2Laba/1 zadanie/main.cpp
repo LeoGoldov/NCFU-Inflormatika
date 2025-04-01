69311121eccb0198c2ea972320129fea686051f2
LeoGoldov <levfeb@gmail.com>
2025-04-01 13:31:21 +0300

    Update main.cpp

--- 2Laba/1 zadanie/main.cpp
+++ 2Laba/1 zadanie/main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include "Header.h"
+using namespace std;
+// Основная функция программы
+int main() {
+    setlocale(LC_ALL, "RU");
+    double calories; 
+    double weight;    
+    cout << "Введите калорийность на 100г продукта: ";
+    cin >> calories;
+    cout << "Введите вес продукта в граммах: ";
+    cin >> weight;
+    Product myProduct(calories, weight); 
+    cout << "Общая калорийность продукта: " << myProduct.calculate() << endl;
+    return 0;
+}
 
