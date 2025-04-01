ca0ddac59e948d2fd15729c05d6b278f05480a43
LeoGoldov <levfeb@gmail.com>
2025-04-01 13:32:14 +0300

    Update Source.cpp

--- 2Laba/1 zadanie/Source.cpp
+++ 2Laba/1 zadanie/Source.cpp
@@ -1,1 +1,9 @@
+#include "Header.h"
+Product::Product(double c, double m) {
+    calories100g = c;
+    weight = m;
+}
+double Product::calculate() {
+    return (calories100g / 100.0) * weight;
+}
 
